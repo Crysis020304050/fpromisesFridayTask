129ef8b2606eed5f165a21b5a082585dd7c481c0
crysis <crysiswarhead020304050@gmail.com>
2019-12-06 16:59:04 +0200

    Last change in the class

---
+++ assets/js/index.js
@@ -0,0 +1,75 @@
+'use strict';
+
+fetch('./user.json')
+.then(response => response.json())
+.then(appendUserItemList)
+.catch(console.error);
+
+function createUserItem(user) {
+
+    const userItemContainer = document.createElement("li");
+    userItemContainer.classList.add('userItem');
+    const userImageContainer = document.createElement("div");
+    userImageContainer.classList.add("imageContainer");
+    const userImage = document.createElement("img");
+
+    userImage.setAttribute("src", `${user.picturePath}`);
+    userImage.onerror = () => {
+
+    };
+
+    userImage.setAttribute("alt", "user picture");
+    userImageContainer.appendChild(userImage);
+
+    userItemContainer.appendChild(userImageContainer);
+
+    const userFullNameElem = document.createElement("h3");
+    userFullNameElem.innerText = `${user.name} ${user.surname}`;
+
+    const userAgeElem = document.createElement("h4");
+    userAgeElem.innerText = user.age;
+    userItemContainer.appendChild(userFullNameElem);
+    userItemContainer.appendChild(userAgeElem);
+
+
+
+    return userItemContainer;
+}
+
+function appendUserItemList(users) {
+
+    users.forEach( user => {
+        document.getElementById("usersList").appendChild(createUserItem(user));
+    });
+
+}
+
+/*function checkUserPictureLink(user) {
+    const linkPromise = new Promise( function (resolve, reject) {
+
+        if (new Image(user.picturePath).onload) {
+            resolve();
+        }
+        reject();
+
+    });
+
+    linkPromise
+        .then(console.log)
+        .catch(console.error);
+
+
+}*/
+
+ function stringToColor(str) {
+    let hash = 0;
+    for (let i = 0; i < str.length; i++) {
+        hash = str.charCodeAt(i) + ((hash << 5) - hash);
+    }
+    let colour = '#';
+    for (let i = 0; i < 3; i++) {
+        let value = (hash >> (i * 8)) & 0xFF;
+        colour += ('00' + value.toString(16)).substr(-2);
+    }
+    return colour;
+}
